bf227d65b250ef5886662d4a1e579394f18869c6
DaveL17 <dave.lehrer@icloud.com>
2018-07-18 03:48:05 -0500

    - More debug logging debugging.

--- GhostXML.indigoPlugin/Contents/Server Plugin/plugin.py
+++ GhostXML.indigoPlugin/Contents/Server Plugin/plugin.py
@@ -41,7 +41,7 @@ __build__     = u""
 __copyright__ = u"There is no copyright for the GhostXML code base."
 __license__   = u"MIT"
 __title__     = u"GhostXML Plugin for Indigo Home Control"
-__version__   = u"0.4.08"
+__version__   = u"0.4.09"
 
 # Establish default plugin prefs; create them if they don't already exist.
 kDefaultPluginPrefs = {
